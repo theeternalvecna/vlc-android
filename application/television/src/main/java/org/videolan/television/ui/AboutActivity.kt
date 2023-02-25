500defa154c5071762c4ecda3c2f6aceff6f1009
Dmitry <dgyudin@gmail.com>
2023-02-25 19:28:56 +0100

    AboutActivity.kt local night theme

--- application/television/src/main/java/org/videolan/television/ui/AboutActivity.kt
+++ application/television/src/main/java/org/videolan/television/ui/AboutActivity.kt
@@ -1,13 +1,15 @@
 package org.videolan.television.ui
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import org.videolan.resources.util.applyOverscanMargin
 import org.videolan.television.R
 import org.videolan.vlc.gui.helpers.UiTools
 
-class AboutActivity : FragmentActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about)
         UiTools.fillAboutView(this, window.decorView.rootView)
